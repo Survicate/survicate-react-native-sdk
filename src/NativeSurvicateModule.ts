bcfa439dd59725408e4cd1135324f0ae2f9e3bcf
SurvicatePawelGodlewski <112856109+SurvicatePawelGodlewski@users.noreply.github.com>
2024-06-14 13:38:41 +0200

    5.0.0 (#89) (#90)
    - Removed initialize method from native modules. In consequence Survicate SDK no longer initializes by itself. Added safety checks in Android native module (for existing methods) to prevent crashes in case where this change goes unnoticed.
    - Added setWorkspaceKey method. It is now possible to set workspace key in code without the need to provide it in AndroidManifest nor info.plist
    - Survicate SDK version bump to 5.0.0

--- src/NativeSurvicateModule.ts
+++ src/NativeSurvicateModule.ts
@@ -2,7 +2,6 @@ import type { TurboModule } from "react-native";
 import { TurboModuleRegistry } from "react-native";
 
 export interface Spec extends TurboModule {
-  initialize(): void;
   initializeSdk(): void;
   invokeEvent(eventName: string, eventProperties: Object): void;
   enterScreen(screenName: string): void;
